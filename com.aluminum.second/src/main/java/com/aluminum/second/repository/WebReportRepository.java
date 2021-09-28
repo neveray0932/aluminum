5ca1da85eb37197c4a6ab409440cb2fa3e06fc56
Andy Chen <neveray0932@gmail.com>
2021-09-28 08:59:07 +0800

    second upload

--- com.aluminum.second/src/main/java/com/aluminum/second/repository/WebReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aluminum.second.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aluminum.second.domain.WebReportBean;
-
-public interface WebReportRepository extends JpaRepository<WebReportBean, String> {
-
-}
